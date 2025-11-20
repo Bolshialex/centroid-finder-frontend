fd6156c01de7db01c248e01ce4cc24f4bd4605b9
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-11-20 14:19:23 -0800

    work on preview

--- src/app/videos/preview/[id]/page.jsx
+++ src/app/videos/preview/[id]/page.jsx
@@ -9,15 +9,17 @@ export default function Page() {
   const videoName = "Ball.mp4";
   const [thumbnail, setThumbnail] = useState(null);
   const [color, setColor] = useState("#ffffff");
+  const [threshold, setThreshold] = useState(0)
+  const [args, setArgs] = useState({color : "#ffffff", threshold: 0})
   useEffect(() => {
     const loadThumbnail = async () => {
       const url = await getThumbnail(videoName);
       setThumbnail(url);
     };
-
     loadThumbnail();
   }, []);
 
+  
   const handleEyeDropper = async () => {
     if (!window.EyeDropper) {
       alert("Your browser does not support the EyeDropper API.");
@@ -27,7 +29,7 @@ export default function Page() {
     const eyeDropper = new EyeDropper();
     try {
       const result = await eyeDropper.open();
-      setColor(result.sRGBHex); // set picked color in state
+      setArgs((prev)=>({...prev, color: result.sRGBHex})); // set picked color in state
     } catch (err) {
       console.error("Eyedropper canceled or failed:", err);
     }
@@ -42,7 +44,18 @@ export default function Page() {
         value={color}
         onChange={(e) => setColor(e.target.value)}
       />
-
+      <div>
+      <label htmlFor="threshold">Adjust threshold:</label>
+      <input
+        type="range"
+        id="threshold"
+        min="0"
+        max="150"
+        value={sliderValue}
+        onChange={handleChange}
+      />
+      <p>Current Value: {sliderValue}</p>
+    </div>
       <button onClick={handleEyeDropper}>Pick From Screen</button>
 
       <span>{color}</span>
