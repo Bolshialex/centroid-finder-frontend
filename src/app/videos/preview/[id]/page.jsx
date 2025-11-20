c45b1e87993b3f5fa48a40cbe3911987d56f2d08
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-11-20 14:38:01 -0800

    Add state to color picker and threshold

--- src/app/videos/preview/[id]/page.jsx
+++ src/app/videos/preview/[id]/page.jsx
@@ -9,8 +9,8 @@ export default function Page() {
   const videoName = "Ball.mp4";
   const [thumbnail, setThumbnail] = useState(null);
   const [color, setColor] = useState("#ffffff");
-  const [threshold, setThreshold] = useState(0)
-  const [args, setArgs] = useState({color : "#ffffff", threshold: 0})
+  const [threshold, setThreshold] = useState(0);
+  const [args, setArgs] = useState({ color: "#ffffff", threshold: 0 });
   useEffect(() => {
     const loadThumbnail = async () => {
       const url = await getThumbnail(videoName);
@@ -19,7 +19,6 @@ export default function Page() {
     loadThumbnail();
   }, []);
 
-  
   const handleEyeDropper = async () => {
     if (!window.EyeDropper) {
       alert("Your browser does not support the EyeDropper API.");
@@ -29,10 +28,20 @@ export default function Page() {
     const eyeDropper = new EyeDropper();
     try {
       const result = await eyeDropper.open();
-      setArgs((prev)=>({...prev, color: result.sRGBHex})); // set picked color in state
+      setArgs((prev) => ({ ...prev, color: result.sRGBHex })); // set picked color in state
     } catch (err) {
       console.error("Eyedropper canceled or failed:", err);
     }
+  };
+
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    if (name == "color") {
+      handleEyeDropper;
+    }
+    setArgs((prev) => ({ ...prev, [name]: value }));
+
+    console.log(args);
   };
 
   return (
@@ -41,24 +50,26 @@ export default function Page() {
       <img src={thumbnail} alt={`${videoName} thumbnail`}></img>
       <input
         type="color"
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
-      />
-      <div>
-      <label htmlFor="threshold">Adjust threshold:</label>
-      <input
-        type="range"
-        id="threshold"
-        min="0"
-        max="150"
-        value={sliderValue}
+        name="color"
+        id="color"
+        value={args.color}
         onChange={handleChange}
       />
-      <p>Current Value: {sliderValue}</p>
-    </div>
+      <div>
+        <label htmlFor="threshold">Adjust threshold:</label>
+        <input
+          type="range"
+          id="threshold"
+          name="threshold"
+          min="0"
+          max="150"
+          value={args.threshold}
+          onChange={handleChange}
+        />
+        <p>Current Value: {args.threshold}</p>
+        <p>Color Value: {args.color}</p>
+      </div>
       <button onClick={handleEyeDropper}>Pick From Screen</button>
-
-      <span>{color}</span>
     </div>
   );
 }
